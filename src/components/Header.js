7183202c08bd533a8670c8bb47c546aa179aba4f
mjaydenkim <mjaydenkim@gmail.com>
2019-04-23 09:01:54 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,8 @@ import logo from '../assets/images/logo.svg';
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
-        <h1>Stellar</h1>
-        <p>Just another free, fully responsive site template<br />
-        design by <a href="https://html5up.net">HTML5 UP</a>.</p>
+        <h1>Zion National Park</h1>
+        <p>This is a school project about Zion National Park and Rock formations.</p>
     </header>
 )
 
