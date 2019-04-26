506560a6b0c1a4e074fdf2b0da25d602efffd2cc
mjaydenkim <mjaydenkim@gmail.com>
2019-04-25 20:23:35 -0700

    Update generic.js

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -10,68 +10,70 @@ class Generic extends React.Component {
 
     return (
       <Layout>
-        <Helmet title="Generic Page Title" />
-        <HeaderGeneric />
+        <Helmet title="Rock Formation Historian" />
+        <header id="header">
+          <h1>Rock Formation Historian</h1>
+          <p>This part was created by Matthew.</p>
+      </header>
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={pic04} alt="" /></span>
               <h1 id="sedimentation">Sedimentation:</h1>
+                <ul>
+                <li>240M years ago, Zion was a relatively flat basin near sea level</li>
 
-<ul>
-<li>240M years ago, Zion was a relatively flat basin near sea level</li>
+                <li>Sand, gravel, mud eroded from surrounding mts.</li>
 
-<li>Sand, gravel, mud eroded from surrounding mts.</li>
+                <li>Streams carried these materials into the basin</li>
 
-<li>Streams carried these materials into the basin</li>
+                <li>Deposited in layers</li>
 
-<li>Deposited in layers</li>
+                <li>The weight caused the basin to sink</li>
 
-<li>The weight caused the basin to sink</li>
+                <li>Top layer kept at sea level</li>
 
-<li>Top layer kept at sea level</li>
+                <li>Massive sedimentation collection created ~10K feet deep</li>
+                </ul>
 
-<li>Massive sedimentation collection created ~10K feet deep</li>
-</ul>
+                <h1 id="lithification">Lithification:</h1>
 
-<h1 id="lithification">Lithification:</h1>
+                <ul>
+                <li>Waters with minerals filtered through sediments</li>
 
-<ul>
-<li>Waters with minerals filtered through sediments</li>
+                <li>Iron oxide, Calcium carbonate, Silica- cementing agents</li>
 
-<li>Iron oxide, Calcium carbonate, Silica- cementing agents</li>
+                <li>Transformed deposits into different kinds of stone</li>
 
-<li>Transformed deposits into different kinds of stone</li>
+                <li>“Ancient seabeds became limestone; mud and clay became mudstones and shale, and desert sand became sandstone.”- NPS</li>
+                </ul>
 
-<li>“Ancient seabeds became limestone; mud and clay became mudstones and shale, and desert sand became sandstone.”- NPS</li>
-</ul>
+                <h1 id="uplift">Uplift</h1>
 
-<h1 id="uplift">Uplift</h1>
+                <ul>
+                <li>Forces deep within Earth push its surface up</li>
 
-<ul>
-<li>Forces deep within Earth push its surface up</li>
+                <li>Slow hoisting of huge blocks of crust</li>
 
-<li>Slow hoisting of huge blocks of crust</li>
+                <li>Zion’s elevation rose from sea level to ~10K feet high</li>
 
-<li>Zion’s elevation rose from sea level to ~10K feet high</li>
+                <li>Uplift is still occurring today</li>
+                </ul>
 
-<li>Uplift is still occurring today</li>
-</ul>
+                <h1 id="erosion">Erosion</h1>
 
-<h1 id="erosion">Erosion</h1>
+                <ul>
+                <li>Zion uplift caused streams to tumble off the plateau, flowing rapidly down a steep gradient</li>
 
-<ul>
-<li>Zion uplift caused streams to tumble off the plateau, flowing rapidly down a steep gradient</li>
+                <li>Streams (which had sediment and boulders) eroded and cut into rock layers</li>
 
-<li>Streams (which had sediment and boulders) eroded and cut into rock layers</li>
+                <li>This formed deep, narrow canyons</li>
 
-<li>This formed deep, narrow canyons</li>
+                <li>North Fork of Virgin River has carried away a lot of rock</li>
 
-<li>North Fork of Virgin River has carried away a lot of rock</li>
+                <li>The Virgin River still excavating</li>
 
-<li>The Virgin River still excavating</li>
-
-<li>Still widening/creating canyons</li>
-</ul>
+                <li>Still widening/creating canyons</li>
+                </ul>
           </section>
         </div>
       </Layout>
