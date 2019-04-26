0c288d158435fdd26b113cf692b02fb2b3280af0
mjaydenkim <mjaydenkim@gmail.com>
2019-04-25 20:15:05 -0700

    Update generic.js

--- src/pages/generic.js
+++ src/pages/generic.js
@@ -15,11 +15,63 @@ class Generic extends React.Component {
         <div id="main">
           <section id="content" className="main">
             <span className="image main"><img src={pic04} alt="" /></span>
-            <h2>Magna feugiat lorem</h2>
-            <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis fames ac ante ipsum primis in faucibus.</p>
-            <p>Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Consequat leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit enim turpis lorem ipsum dolor sit amet feugiat. Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus, lacus eget hendrerit bibendum, urna est aliquam sem, sit amet est velit quis lorem.</p>
-            <h2>Tempus veroeros</h2>
-            <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
+              <h1 id="sedimentation">Sedimentation:</h1>
+
+<ul>
+<li>240M years ago, Zion was a relatively flat basin near sea level</li>
+
+<li>Sand, gravel, mud eroded from surrounding mts.</li>
+
+<li>Streams carried these materials into the basin</li>
+
+<li>Deposited in layers</li>
+
+<li>The weight caused the basin to sink</li>
+
+<li>Top layer kept at sea level</li>
+
+<li>Massive sedimentation collection created ~10K feet deep</li>
+</ul>
+
+<h1 id="lithification">Lithification:</h1>
+
+<ul>
+<li>Waters with minerals filtered through sediments</li>
+
+<li>Iron oxide, Calcium carbonate, Silica- cementing agents</li>
+
+<li>Transformed deposits into different kinds of stone</li>
+
+<li>“Ancient seabeds became limestone; mud and clay became mudstones and shale, and desert sand became sandstone.”- NPS</li>
+</ul>
+
+<h1 id="uplift">Uplift</h1>
+
+<ul>
+<li>Forces deep within Earth push its surface up</li>
+
+<li>Slow hoisting of huge blocks of crust</li>
+
+<li>Zion’s elevation rose from sea level to ~10K feet high</li>
+
+<li>Uplift is still occurring today</li>
+</ul>
+
+<h1 id="erosion">Erosion</h1>
+
+<ul>
+<li>Zion uplift caused streams to tumble off the plateau, flowing rapidly down a steep gradient</li>
+
+<li>Streams (which had sediment and boulders) eroded and cut into rock layers</li>
+
+<li>This formed deep, narrow canyons</li>
+
+<li>North Fork of Virgin River has carried away a lot of rock</li>
+
+<li>The Virgin River still excavating</li>
+
+<li>Still widening/creating canyons</li>
+</ul>
           </section>
         </div>
       </Layout>
